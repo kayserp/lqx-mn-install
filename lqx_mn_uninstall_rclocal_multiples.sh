2c326929b89d5b49ef0154da6031f43b1866e7c7
Kayser <gerkayser@hotmail.com>
2020-05-18 16:34:35 -0300

    .

--- lqx_mn_uninstall_rclocal_multiples.sh
+++ lqx_mn_uninstall_rclocal_multiples.sh
@@ -1,10 +1,9 @@
 rm -rf /tmp/lqxcore-linux/
-
-userdel nm01
 rm -rf /home/nm01
-userdel nm02
 rm -rf /home/nm02
-
 rm -rf etc/rc.local
 rm -rf /etc/fail2ban/jail.local
 rm -rf /etc/cron.d/lqx_sentinel
+userdel nm01
+userdel nm02
+
